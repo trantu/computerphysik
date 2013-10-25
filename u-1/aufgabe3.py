741caedaf117805db4fe89bf8d17545ae3041280
Carlos Martín Nieto <cmn@dwim.me>
2013-10-25 16:05:33 +0200

    U1: A3 add comments

--- u-1/aufgabe3.py
+++ u-1/aufgabe3.py
@@ -26,25 +26,34 @@ def bench(fn):
 res = [(numpy.cos(n), tcos(n, 15)) for n in [0.0, 1.0, math.pi, 2*math.pi]]
 
 for val in res:
-       print "%f\t%f" % val
+       print("%f\t%f" % val)
 
 ## Now let's compare the results of tcos with numpy.cos
 
-# These are always the same
+# These lists are constant for all of the plots
 xs = numpy.arange(0, 2*math.pi, 0.1)
 theirs = [numpy.cos(n) for n in xs]
+
+# Return tcos(x, N) values for our list
 def splot(n):
     return [tcos(x, n) for x in xs]
 
-plots = [(1, 231), (3, 232), (5, 233), (7, 234), (9, 235), (11, 236)]
+# Calculate the pairweise difference between two lists
+def diff(a, b):
+    return [abs(a - b) for (a, b) in zip(ours, theirs)]
+
+# Store the instructions for the plots. Each tuple stores N and a
+# tuple with the subplot we want to draw it on
+plots = [(1, (2,3,1)), (3, (2,3,2)), (5, (2,3,3)), (7, (2,3,4)),
+         (9, (2,3,5)), (11, (2,3,6))]
 for (n, location) in plots:
-    plot.subplot(location)
+    plot.subplot(*location)
     plot.title("n = %d" % n)
 
     ours = splot(n)
     plot.plot(xs, ours)
     plot.plot(xs, theirs)
-    plot.plot(xs, [abs(a - b) for (a, b) in zip(ours, theirs)])
+    plot.plot(xs, diff(ours, theirs))
 
     plot.xlabel(u'x')
     plot.ylabel(u'cos(x)')
@@ -52,13 +61,14 @@ for (n, location) in plots:
     plot.legend((u'$t\cos_N(x)$', u'$\cos(x)$', u'Fehler'), loc='lower left')
     plot.axis([0, 2*math.pi, -1, 1])
 
-#plot.show()
+plot.show()
 
 ## Let's benchmark it
 
-ours = [bench(lambda: tcos(0, n)) for n in range(0, 1000, 10)]
-theirs = [bench(lambda: numpy.cos(n)) for n in range(0, 1000, 10)]
+rng = range(0, 1000, 10)
+ours = [bench(lambda: tcos(0, n)) for n in rng]
+theirs = [bench(lambda: numpy.cos(n)) for n in rng]
 plot.subplot(111)
-plot.plot(range(0, 1000, 10), [t for (_, t) in ours])
-plot.plot(range(0, 1000, 10), [t for (_, t) in theirs])
-#plot.show()
+plot.plot(rng, [t for (_, t) in ours])
+plot.plot(rng, [t for (_, t) in theirs])
+plot.show()
