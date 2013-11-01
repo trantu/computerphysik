b1656446b3b7a502f6fb90f2237dedb9a14f630a
Carlos Martín Nieto <cmn@dwim.me>
2013-11-01 17:48:36 +0100

    U2 Newton

--- u-2/u2_3.py
+++ u-2/u2_3.py
@@ -4,6 +4,7 @@
 # Carlos Martín Nieto, Tu Tran
 
 import math
+from scipy.misc import derivative
 
 # return 1 if v is positive, 0 otherwise
 def sig(v):
@@ -44,10 +45,12 @@ def bisection(f, a, b, delta):
 
     # Keep getting more accurate invervals until the differences are
     # smaller than delta
+    n = 0
     while not tuple_diff(interval) < delta:
         interval = next_interval(f, interval)
+        n += 1
 
-    return interval
+    return (interval, n)
 
 def fixed_point_iteration(f, x0, delta):
     """
@@ -57,13 +60,49 @@ def fixed_point_iteration(f, x0, delta):
     last_point = x0
     point = f(x0)
 
+    n = 0
     while not abs(point - last_point) < delta:
         last_point = point
         point = f(last_point)
+        n += 1
 
-    return point
+    return (point, n)
 
+def next_point(f, x):
+    """ Return the next point for Newton """
+    deriv = derivative(f, x)
+    return x - (f(x)/deriv)
+
+def newton(f, x0, delta):
+    """
+    Return the zero with `delta` precision
+    """
+
+    last_point = x0
+    point = next_point(f, x0)
+
+    n = 0
+    while not abs(last_point - point) < delta:
+        last_point = point
+        point = next_point(f, last_point)
+        n += 1
+
+    return (point, n)
+
+## Sanity checking
 f = lambda x: math.cos(x)**2 - x + 0.2
 fi = lambda x: math.cos(x)**2 + 0.2
-print("bisection (%g, %g)" % bisection(f, -1.0, 1.0, 0.01))
-print("fixed point iter %g" % fixed_point_iteration(fi, 0.0, 0.01))
+print("For f(x):")
+((a,b), n) = bisection(f, -1.0, 1.0, 0.00001)
+print("\tbisection (%g, %g), %g" % (a,b,n))
+print("\tfixed point iter %g, %d" % fixed_point_iteration(fi, 0.0, 0.01))
+print("\tNewton iter %g, %d" % newton(f, 0.0, 0.01))
+
+g = lambda x: x**2 - x - 0.2
+gi = lambda x: x**2 - 0.2
+print("For g(x):")
+#print("\tbisection (%g, %g)" % bisection(g, -1.0, 1.0, 0.01))
+print("\tfixed point iter %g, %d" % fixed_point_iteration(gi, 0.0, 0.0000000001))
+print("\tNewton iter %g, %d" % newton(g, 0.0, 0.000000000000001))
+
+deltas = [2**-x for x in range(1, 21)]
