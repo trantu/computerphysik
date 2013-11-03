a6b2d79633bba369df0354b27cfc4a6f41ef7ccf
tran <tran@tran.(none)>
2013-11-03 21:18:46 +0100

    changed for snd derivation

--- u-2/u2_2.py
+++ u-2/u2_2.py
@@ -53,9 +53,10 @@ Wenn k >= 5 wird der Fehler kleiner sein als 0.01, deswegen waehlen wir k = 5 fu
 delta_x_k = 2*math.pi/2**5
 #Intervall fuer normale Funktion
 interval = numpy.arange(-math.pi, math.pi+delta_x_k, delta_x_k)
-#Intervall fuer diff_quotient
+#Intervall fuer diff_quotient der 1ten Ableitung
 interval_diff = numpy.arange(-math.pi+delta_x_k, math.pi, delta_x_k)
-
+#Intervall fuer diff_quotient der 2ten Ableitung
+interval_diff_2te_Ableitung = numpy.arange(-math.pi+delta_x_k*2, math.pi-delta_x_k, delta_x_k)
 # 1. Ableitung von cos soll durch diff_quotient angenaehert werden
 ys = map(math.cos, interval)
 res1, abs_fehler = diff_quot(cos_1_ableitung, interval, ys)
@@ -66,13 +67,13 @@ plot.plot(interval_diff, res_b1)
 plot.plot(interval_diff, res_a1)
 
 # 2. Ableitung von cos soll durch diff_quotient angenaehert werden
-ys = map(cos_1_ableitung, interval)
-res2, abs_fehler2 = diff_quot(cos_2_ableitung, interval, ys)
+ys = res_a1
+res2, abs_fehler2 = diff_quot(cos_2_ableitung, interval_diff, ys)
 
 res_a2, res_b2 = zip(*res2)
 
-plot.plot(interval_diff, res_a2)
-plot.plot(interval_diff, res_b2)
+plot.plot(interval_diff_2te_Ableitung, res_a2)
+plot.plot(interval_diff_2te_Ableitung, res_b2)
 
 plot.legend(("$\cos'(x)$", "$-\sin(x)$", "$-\sin'(x)$", "$-\cos(x)$"))
 plot.xlabel("$x$")
