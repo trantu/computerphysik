88a72d24b7e06bf3c592418522e0eee5b5512a4f
Carlos Martín Nieto <cmn@dwim.me>
2013-11-08 22:20:49 +0100

    U3 LR

--- u-3/u3_2.py
+++ u-3/u3_2.py
@@ -3,8 +3,53 @@
 
 # Carlos Martín Nieto, Tu Tran
 
-from numpy import matrix, zeros
+from numpy import matrix, zeros, eye
 from math import sqrt
+
+def alpha(a, pivot):
+    return -1*(a/pivot)
+
+def swap(m, a, b):
+    temp = matrix(m[a])
+    m[a] = m[b]
+    m[b] = temp
+
+# first, d is ignored
+def gauss_alg(F, d):
+    zeilen,spalten = F.shape
+    # nicht moeglich zu eliminieren
+    if zeilen != spalten or zeilen == 0 or spalten == 0:
+        raise ValueError("ungleiche Anzahl zwischen Zeilen und Spalten")
+
+    L = []
+
+    # axis = 1 -> vertical
+    #F = concatenate((F, d.T), axis=1)
+    for s in range(spalten):
+        for z in range(s,zeilen):
+            pivot = F[z,z]
+
+            #Zeilenvertauschen
+            if pivot == 0:
+                for ii in range(z+1,zeilen):
+                    if F[ii,s] != 0:
+                        swap(F, z, ii)
+                        break # back to second loop
+
+            pivot = F[z,z]
+            for zz in range(z+1,zeilen):
+                if F[zz,s] != 0:
+                    #print F[z,:]
+                    #print F[z+1,:]
+                    alp = alpha(F[zz,s], pivot)
+                    F[zz] = (alp * F[z]) + F[zz]
+                    l = eye(zeilen, spalten)
+                    l[zz] = (alp * l[z]) + l[zz]
+                    L.append(l)
+
+    LL = reduce(lambda acc, x: x.dot(acc), L)
+
+    return F, LL
 
 def cholesky(A):
     n, m = A.shape
@@ -38,3 +83,13 @@ if (R.transpose().dot(R) == A).all():
     print "\nCalculations correct: R^T * R = A ✔"
 else:
     print "Oh no! R^T * R != A"
+
+F, L = gauss_alg(matrix(A), None)
+
+print F, L
+
+# print "control"
+
+# F, L = gauss_alg(matrix('1 2 3; 6 -2 2; -3 1 -4', dtype=float), None)
+
+# print L
