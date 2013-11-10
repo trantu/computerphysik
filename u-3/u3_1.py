2a9867a19f0472840b76718bb3e8c84b6b25cc2b
Tran Tu <trantu456@gmail.com>
2013-11-10 17:41:09 +0100

    u3_1.py completed

--- u-3/u3_1.py
+++ u-3/u3_1.py
@@ -7,60 +7,135 @@ Created on Nov 8, 2013
 import math
 import copy
 from numpy import * 
+from copy import deepcopy
+
+
 def mround ( x,N ) :
-    if ( x==0 ) :
+    if ( x== 0.0 ) :
         return x
     return round ( x, int ( N - math.ceil ( math.log10 ( abs ( x )))))
-
 def alpha(a,pivot):
     return -1*(a/pivot)
 
-def swap(m, a, b):
+def tausch_Zeilen(m, a, b):
     temp = matrix(m[a])
     m[a] = m[b]
     m[b] = temp
-
-# first, d is ignored
-def gauss_alg(F, d):
-    zeilen,spalten = shape(F)
-    # nicht moeglich zu eliminieren
-    if zeilen != spalten or zeilen == 0 or spalten == 0:
+def eliminable(A,b):
+    zeilen,spalten = shape(A)
+    if (zeilen != spalten or zeilen == 0 or spalten == 0):
         raise ValueError("ungleiche Anzahl zwischen Zeilen und Spalten")
+    for i in range(zeilen):
+        if mean(A[i,:]) == 0.0:
+            raise ValueError("eine Zeile Null")
+    return 1
+#rueckwaerts loesen
+def solve (F, d,N):
+    A = deepcopy(F)
+    b = deepcopy(d)
+    zeilen,_ = shape(A)
+    rng = range(zeilen)
+    rng.reverse()
+    for i in rng:
+        for j in rng:
+            if j == i:
+                A[i,j] = mround(b[i]/A[i,j],N)
+                for ii in range(i):
+                    A[ii,j] = mround(A[ii,j]*A[i,j],N)
+                break
+            b[i] = mround(b[i] - A[i,j],N)
+            A[i,j] = 0.0
+    print 'Result: [X1 X2 X3]'
+    print diag(A)
+    return diag(A)
 
-    # axis = 1 -> vertical
-    #F = concatenate((F, d.T), axis=1)
+def gauss_alg_mround(A, b, N):
+    F = deepcopy(A)
+    d = deepcopy(b)
+    zeilen,spalten = shape(F)
+    d = d.T
     for s in range(spalten):
-        for z in range(s,zeilen):
-            pivot = F[z,z]
-            #Zeilenvertauschen
-            if pivot == 0:
-                for ii in range(z+1,zeilen):
-                    if F[ii,s] != 0:
-                        swap(F, z, ii)
-                        break # back to second loop
-                    
-            pivot = F[z,z]
-            #print pivot
-            for zz in range(z+1,zeilen):    
-                if F[zz,s] != 0:
-                    #print F[z,:]
-                    #print F[z+1,:]
-                    alp = alpha(F[zz,s], pivot)
-                    F[zz] = (alp * F[z]) + F[zz]
-    return F
+        #Ueberpruefen, ob eliminierbar
+        eliminable(F, d)
+        pivot = F[s,s]
+        #Zeilenvertauschen
+        if(pivot == 0):
+            for ii in range(s+1,zeilen):
+                if(F[ii,s] != 0):
+                    tausch_Zeilen(F, s, ii)
+                    tausch_Zeilen(d, s, ii)
+                    break
+        pivot = F[s,s]
+        for zz in range(s+1,zeilen):  
+            #addition der Zeilen  
+            if F[zz,s] != 0:
+                alp = alpha(F[zz,s], pivot)
+                d[zz] = mround((alp* d[s])+d[zz], N)
+                for i in range(s,spalten):
+                    F[zz,i] = mround((alp * F[s,i]) + F[zz,i], N)
+    print 'Matrix A'
+    print F
+    print 'Rechte Seite b'
+    print d
+    return F,d
 '''
-Test
+# 3.1.2: Im Vergleich zu dem Verfahren ohne mround mit N = 4 sieht man mit der Rundenfunktion, dass die Zahlen an der 4. Stelle nach dem Komma
+gerundet werden 
 '''
-#m = matrix('1 2 3; 1 1 1; 3 3 1')
-#d = matrix('2 2 0')
+# Suche max Element in einer Spalte und gebe die Zeilennummer zuruek
+def max_pivot(spalte,zeilennummer):
+    a,_ = shape(spalte)
+    zeile = 0
+    maxi = 0.0
+    for i in range(zeilennummer,a):
+        if abs(spalte[i]) > abs(maxi):
+            maxi = spalte[i]
+            zeile = i
+    return zeile
+
+def gauss_alg_mround_best_pivot(A, b, N):
+    F = deepcopy(A)
+    d = deepcopy(b)
+    zeilen,spalten = shape(F)
+    d = d.T
+    for s in range(spalten):
+        eliminable(F, d)
+        getauscht = False
+        if ~getauscht:
+            zeile = max_pivot(F[:,s],s)
+            # tausche max pivot
+            if zeile != s:
+                tausch_Zeilen(F, s, zeile)
+                tausch_Zeilen(d, s, zeile)
+            getauscht = True
+        #print F    
+        pivot = F[s,s]
+        for zz in range(s+1,zeilen):  
+            #addition der Zeilen  
+            if F[zz,s] != 0:
+                alp = alpha(F[zz,s], pivot)
+                d[zz] = mround((alp* d[s])+d[zz], N)
+                for i in range(s,spalten):
+                    F[zz,i] = mround((alp * F[s,i]) + F[zz,i], N)
+    print 'Matrix A'
+    print F
+    print 'Rechte Seite b'
+    print d
+    return F, d
+        
 m = matrix ('1 -2 -1; 36000 2 0; -2 1400 1',dtype = float);
 d = matrix ('3 72002 1399',dtype = float);
-'''
-Ausgabe:
-[[  1.00000000e+00  -2.00000000e+00  -1.00000000e+00   3.00000000e+00]
- [  0.00000000e+00   7.20020000e+04   3.60000000e+04  -3.59980000e+04]
- [  0.00000000e+00   0.00000000e+00  -6.98980612e+02   2.10294183e+03]]
-'''
-print gauss_alg(m, d)
 
+print '##### 3.1.2 #####'
+# N = 20 ist nur symbolisch. Es soll hier nicht gerundet werden.
+m1,d1 = gauss_alg_mround(m, d, 20)
+solve(m1,d1,10)
 
+print '##### 3.1.2 #####'
+print 'matrix mit mround , N = 4'
+m2,d2 = gauss_alg_mround(m, d, 4)
+solve(m2,d2,4)
+print '##### 3.1.3 #####'
+print 'matrix mit mround , N = 4'
+m3,d3 = gauss_alg_mround_best_pivot(m, d, 4)
+solve(m3,d3,4)
