e64a4ff9e367ff48f5e59af3638e9bb01fa17342
Carlos Martín Nieto <cmn@dwim.me>
2013-12-13 17:52:04 +0100

    U8: finish 1b

--- u-8/u8_1.py
+++ u-8/u8_1.py
@@ -11,10 +11,8 @@ import matplotlib.pyplot as plt
 def euler(n, h, y0, F, t0=0):
     y, tn = float(y0), float(t0)
     ys = []
-    print 'h', h
     for i in range(1, n+1):
         y = y + h*F(tn, y)
-        #print 'euler y', y, 'tn', tn
         ys.append(y)
         tn = tn + h
 
@@ -42,7 +40,6 @@ def runge_kutta(n, h, y0, F, t0=0):
         K4 = F(tn + h,  y + h*K3)
         y = y + (h/6.0)*(K1 + 2*K2 + 2*K3 + K4)
         ys.append(y)
-        print 'tn', tn, 'y', ys[-1]
         tn = tn + h
 
     return ys
@@ -71,21 +68,41 @@ def f(t, y):
     return 3.0*y - 4.0*exp(-t)
 
 for h in hs:
+    # calcultate n for the range
+    n = int((trange[1] - trange[0])/h) + 1
+    ts = linspace(0, 2, n)
     for (lbl, fn, opt) in funs:
-        # calcultate n for the range
-        n = int((trange[1] - trange[0])/h) + 1
-        ts = linspace(0, 2, n)
         ys = fn(n, h, 1, f)
-        plt.plot(ts, ys, opt, label=lbl + ' h = %f' % h)
-    plt.legend()
+        plt.plot(ts, ys, opt, label=lbl + ' h = %.3f' % h)
 
 # calcultate n for the range
 h = 0.1
 n = int((trange[1] - trange[0])/h) + 1
 ts = linspace(0, 2, n)
-ys = euler(n, h, 1, f)
-plt.plot(ts, ys, '--', label='Exakt')
+ys = exact(n, h, 1, f)
+plt.plot(ts, ys, '.', label='Exakt')
 
+plt.title("Loesungen von $y'(t) = 3y - 4e^{-t}$")
+plt.ylabel("$y'(t)$")
+plt.xlabel("$x$")
+plt.legend(loc='lower left')
 plt.show()
-        
-    
+
+# let's for for b)
+
+trange = (0, 1)
+for (lbl, fn, opt) in funs:
+    err = []
+    for h in hs:
+        n = int((trange[1] - trange[0])/h) + 1
+        ts = linspace(0, 1, n)
+        truth = exact(n, h, 1, f)[-1]
+        y = fn(n, h, 1, f)[-1]
+        err.append(abs(y - truth))
+    plt.loglog(hs, err, label=lbl)
+
+plt.title('Fehler an $t=1$')
+plt.xlabel('$h$')
+plt.ylabel('Fehler')
+plt.legend()
+plt.show()
