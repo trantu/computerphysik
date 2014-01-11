58adecf2cf8ac6ffdf56d18ea4446a6fce666d7f
Carlos Martín Nieto <cmn@dwim.me>
2014-01-11 17:37:02 +0100

    U10.1.2: working better

--- u-10/u10_exp_euler.py
+++ u-10/u10_exp_euler.py
@@ -11,6 +11,8 @@ def u(x, t, U):
     if t == 0:
         return U/2.0 * exp(-abs(x)*U)
 
+    raise NotImplementedError
+
 def exp_euler(f, tN, dt, xN, dx):
     """Explicit Euler for particle diffussion
     f: function to use
@@ -18,24 +20,21 @@ def exp_euler(f, tN, dt, xN, dx):
     dx: difference in space
     """
 
-    mat = np.matrix(np.zeros((tN+1, xN)), dtype=float)
+    mat = np.matrix(np.zeros((xN, tN)), dtype=float)
     r = dt / dx**2
 
     # Initial values with u(x, t=0)
-    for n in range(xN):
-        mat[0,n] = f(n * dx, 0)
+    for i in range(xN):
+        mat[i,0] = f(i * dx, 0)
 
     print mat
 
-    for i in range(1, tN):
-        # copy over the results from the last
-        mat[i,0] = mat[i-1,1]
-        mat[i,xN-1] = mat[i-1, xN-1]
-        for n in range(1, xN-1):
+    for n in range(1, tN):
+        for i in range(xN-1):
             # since we're calculating u_i^{n+1}, we need to replace n -> n-1 in code
             mat[i,n] = mat[i,n-1] + r * (mat[i,n-1] - 2*mat[i,n-1] + mat[i+1,n-1])
 
-    return mat[-2,:]
+    return mat[:,-1].tolist()
 
 # 10.1.4
 ts = [0, 0.01, 0.03, 0.04, 0.05]
@@ -43,12 +42,13 @@ ts = [0, 0.01, 0.03, 0.04, 0.05]
 U = 10
 tN = 6
 dt = 0.01
-xN = 15
-dx = 1
+xN = 10
+dx = 0.1
 
-[ys] = exp_euler(lambda x, t: u(x, t, U), tN, dt, xN, dx).tolist()
-print ys
+# TODO: try with different dx sizes
+ys = exp_euler(lambda x, t: u(x, t, U), tN, dt, xN, dx)
 r = dt / dx**2
+
 plt.plot(ys, label='r = %f' % r)
 plt.legend()
 plt.show()
