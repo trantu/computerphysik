42f04510175f434031528e35cd4a262e82966bdd
Carlos Martín Nieto <cmn@dwim.me>
2014-01-11 18:21:40 +0100

    U10.1.2: work from -0.5 to 0.5

--- u-10/u10_exp_euler.py
+++ u-10/u10_exp_euler.py
@@ -13,19 +13,22 @@ def u(x, t, U):
 
     raise NotImplementedError
 
-def exp_euler(f, tN, dt, xN, dx):
+def exp_euler(f, tN, dt, xs, dx):
     """Explicit Euler for particle diffussion
     f: function to use
+    tN: how many times to use
     dt: difference in time
+    xs: list of x values
     dx: difference in space
     """
 
+    xN = len(xs)
     mat = np.matrix(np.zeros((xN, tN)), dtype=float)
     r = dt / dx**2
 
     # Initial values with u(x, t=0)
-    for i in range(xN):
-        mat[i,0] = f(i * dx, 0)
+    for i in range(len(xs)):
+        mat[i,0] = f(xs[i], 0)
 
     for n in range(1, tN):
         for i in range(xN-1):
@@ -40,13 +43,14 @@ ts = [0, 0.01, 0.03, 0.04, 0.05]
 U = 10
 tN = 6
 dt = 0.01
-xN = 10
 dx = 0.1
 
-for dx in [0.10, 0.15, 0.20]:
-    ys = exp_euler(lambda x, t: u(x, t, U), tN, dt, xN, dx)
+#for dx in [0.1]:
+for dx in [0.11, 0.15, 0.2, 0.3, 0.4]:
+    xs = np.arange(-0.5, 0.5, dx)
+    ys = exp_euler(lambda x, t: u(x, t, U), tN, dt, xs, dx)
     r = dt / dx**2
-    plt.plot(ys, label='dx = %f, r = %f' % (dx, r))
+    plt.plot(xs, ys, label='dx = %f, r = %f' % (dx, r))
 
 plt.legend()
 plt.show()
