99001703de3325196426d72cd6e88547568ce3c1
Tran Tu <trantu456@gmail.com>
2014-01-18 16:58:19 +0100

    added 11.1.3, noch nicht ganz sicher mit 11.1.3

--- u-11/u11_1.py
+++ u-11/u11_1.py
@@ -7,7 +7,9 @@ Created on Nov 29, 2013
 import time
 import numpy as np
 import matplotlib.pyplot as plt
-
+# Daten einlesen
+data = np.recfromtxt('P.txt')
+(l,_) = np.shape(data)
 # Aufgabe 11.1.1
 def autokorrelation(T, tao, Pdata):
     autokorr = 0.0
@@ -26,8 +28,6 @@ def autokorrelation(T, tao, Pdata):
     return normierung*autokorr
 
 def u11_1_1():
-    data = np.recfromtxt('P.txt')
-    (l,_) = np.shape(data)
     start = time.clock()
     print autokorrelation(l, 0.0, data)
     elapsed = (time.clock() - start)
@@ -40,12 +40,14 @@ def u11_1_1():
     for i in range(0,100,5):
         phis.append(autokorrelation(l, i, data))
     plt.plot(range(0,100,5),phis)
+    plt.title('u11_1_1')
     plt.show()
-# 11.1.1
+
 u11_1_1()
+
+#u11_1_2
 def u11_1_2():
-    data = np.recfromtxt('P.txt')
-    (l,_) = np.shape(data)
+    start = time.clock()
     Px = np.zeros(2*l)
     Py = np.zeros(2*l)
     Pz = np.zeros(2*l)
@@ -64,19 +66,27 @@ def u11_1_2():
     Px = np.fft.ifft(Px)
     Py = np.fft.ifft(Py)
     Pz = np.fft.ifft(Pz)
-    print np.shape(Px)
-    print np.shape(Py)
-    print np.shape(Pz)
-    tao = 10000
+    tau = 10000
     phis = []
-    for j in range(tao):
+    printed = 0
+    for j in range(tau):
         summe = 0.0
         #print range(l-j)
         summe = summe + Px[j]+Py[j]+Pz[j]
-        phis.append(summe.real)
-    print 'bla'
+        normierung = summe.real*(l-j)**(-1)
+        phis.append(normierung)
+        #print normierung     
+        # erste Nullstelle fuer u11_1_3 finden (Obergrenze des Intergrals)
+        if normierung >= 0.0 and normierung <= 0.001 and not printed:
+            print 'print', j
+            printed = 1
+    elapsed = (time.clock() - start)
+    print 'Time u11.1.2: ', elapsed
+    #print phis
     plt.plot(range(1000), phis[0:1000])
+    plt.title('u11_1_2')
     plt.show()
+    # TODO normieren, Zeit messen
     #summe = 0.0
     #for i in range(l):
     #    summe = summe + Px[i]+Py[i]+Pz[i]
@@ -84,3 +94,54 @@ def u11_1_2():
     #print np.fft.ifft(np.fft.fft(data,30,0))
     
 u11_1_2()
+
+def phi(tau = 100000):
+    Px = np.zeros(2*l)
+    Py = np.zeros(2*l)
+    Pz = np.zeros(2*l)
+    Px[0:l] = data[:,0]
+    Py[0:l] = data[:,1]
+    Pz[0:l] = data[:,2]
+    Px = np.fft.fft(Px)
+    Py = np.fft.fft(Py)
+    Pz = np.fft.fft(Pz)
+    
+    for i in range(l):
+        Px[i] = abs(Px[i]*Px[i])
+        Py[i] = abs(Py[i]*Py[i])
+        Pz[i] = abs(Pz[i]*Pz[i])
+        
+    Px = np.fft.ifft(Px)
+    Py = np.fft.ifft(Py)
+    Pz = np.fft.ifft(Pz)
+    phis = []
+    for j in range(tau):
+        summe = 0.0
+        #print range(l-j)
+        summe = summe + Px[j]+Py[j]+Pz[j]
+        normierung = summe.real*(l-j)**(-1)
+        phis.append(normierung)
+    return phis
+def u11_1_3():
+    phis = phi()
+    def X(f):
+        vorfactor = 1.8769
+        phi_0 = phis[0]
+        z1 = -(2*np.pi*f)*1j
+        a = 0
+        b = 1046 # Obergrenze abgelesen von u11_1_2()
+        summe = 0.0
+        for t in range(a,b):
+            summe = summe + np.exp(-2*np.pi*f*t*1j)*phis[t]
+        
+        return vorfactor*(phi_0 - z1*(summe))
+    ys_real = [X(freq).real for freq in np.arange(0.1,100,0.1)]
+    ys_imag = [X(freq).imag for freq in np.arange(0.1,100,0.1)]
+    plt.figure()
+    plt.title('u11.1.3 Real-Teil-Plot')
+    plt.semilogy(np.arange(0.1,100,0.1),ys_real)
+    plt.figure()
+    plt.title('u11.1.3 Imaginaer-Teil-Plot')
+    plt.semilogy(np.arange(0.1,100,0.1),ys_imag)
+    plt.show()
+u11_1_3()
