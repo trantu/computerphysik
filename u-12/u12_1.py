c1287f4b8b015b4c39806691081454c776013931
Carlos Martín Nieto <cmn@dwim.me>
2014-01-23 16:25:10 +0100

    U12.1: attempt at 3

--- u-12/u12_1.py
+++ u-12/u12_1.py
@@ -4,7 +4,7 @@
 # Carlos Martín, Tran Tu
 
 import os, time
-from math import sqrt
+from math import sqrt, cos, pi
 from itertools import islice
 import matplotlib.pyplot as plt
 import numpy as np
@@ -42,24 +42,69 @@ class RandomGenerator:
 
 # 12.1.2
 
+def mean(xs):
+    return sum(xs) / len(xs)
+
+def sigma(xs):
+    xmean = mean(xs)
+    sq_diffs = [(xmean - x)**2 for x in xs]
+    sq_mean = sum(sq_diffs) / len(xs)
+
+    return sqrt(sq_mean)
+
 m = 2**31 - 1
 N = 10000
 
 rng = RandomGenerator()
 xs = [float(r) / m for r in take(rng, N)]
-mean = sum(xs) / N
+
 
 # squared differences from the mean
-sq_diffs = [(mean - x)**2 for x in xs]
-sq_mean = sum(sq_diffs) / N
-stddev = sqrt(sq_mean)
 
-print 'Mean', mean
-print 'σ   ', stddev
+xmean = mean(xs)
+xsig = sigma(xs)
+print 'Mean', xmean
+print 'σ   ', xsig
 
 # Histogramm
 plt.hist(xs, bins=np.arange(0, 1.01, 0.1))
-plt.title(r'Histogram der Zufallszahlen, Mean %f, $\sigma=$%f' % (mean, stddev))
+plt.title(r'Histogramm der Zufallszahlen, Mean %f, $\sigma=$%f' % (xmean, xsig))
 plt.xlabel('Werte')
 plt.ylabel('Anzahl')
+plt.show()
+
+# 10.1.3
+
+def C(xs, ys):
+    if len(xs) != len(ys):
+        raise ValueError
+
+    xmean = mean(xs)
+    xsig = sigma(xs)
+    ymean = mean(ys)
+    ysig = sigma(ys)
+    n = len(xs)
+
+    s = sum([(xs[i] - xmean) * (ys[i] - ymean) / (xsig * ysig) for i in range(n)])
+
+    return s / n
+
+ls = range(100, 10001, 100)
+corrs = []
+for l in ls:
+    xs = take(rng, l)
+    corrs.append(C(xs[:-1], xs[1:]))
+
+plt.plot(ls, corrs, label='VL')
+
+corrs = []
+for l in ls:
+    xs = take(rng, l)
+    xcos = [cos((pi/180)*j + xj) for (xj, j) in zip(xs, range(1, len(xs)+1))]
+    corrs.append(C(xcos[:-1], xcos[1:]))
+
+plt.plot(ls, corrs, label='cos')
+
+plt.title('Korrelation zwischen Zufallszahlen')
+plt.legend()
 plt.show()
