c3a2ddb310917fc7b31fb7b538a97da596373d36
Tran Tu <trantu456@gmail.com>
2014-01-24 15:43:02 +0100

    add u12_2

--- u-12/u12_2.py
+++ u-12/u12_2.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''
 Created on Nov 29, 2013
@@ -45,20 +44,29 @@ def stuetz_monteC_integr(f,N,a,b):
     return vorfaktor*summe
 
 def hit_miss_monte(f,fmin,fmax,N,a,b):
-    N_plus = 0
+    N_plus = 0.0
     r = RandomGenerator()
     for _ in range(N):
         ri = float(r.next())/(2**31 - 1)
         si = float(r.next())/(2**31 - 1)
         if f(a+(b-a)*ri) - fmin > (fmax-fmin)*si:
             N_plus = N_plus + 1
+    return (b-a)*((N_plus/np.float(N))*(fmax - fmin) + fmin)
+
 def f(x):
-    return np.abs(np.pi*x)            
+    return np.abs(np.pi*x)
+            
 def mittelpunkt(f,a,b,N):
     vorfaktor = np.float((b-a)/N)
     summe = 0.0
     for i in range(1,N):
         summe = summe + f(a+(b-a)*(i-0.5)/np.float(N))
     return vorfaktor*summe
+def u12_2():
+    N = 2**16
+    ns = [2**(x) for x in range(-1,17)]
 if __name__ == '__main__':
-    pass
+    r = RandomGenerator()
+    for _ in range(10):
+        print float(r.next())/(2**31 - 1)
+#!
