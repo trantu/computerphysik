73aa4f85297d8158f07ca07c21442c46eca8eefc
Carlos Martín Nieto <cmn@dwim.me>
2014-01-24 18:19:19 +0100

    U12.2: solve 3, with plotting

--- u-12/u12_3.py
+++ u-12/u12_3.py
@@ -43,7 +43,7 @@ class UniformRNG(RandomGenerator):
 import itertools
 import numpy as np
 import matplotlib.pyplot as plt
-from math import exp
+from math import exp, sqrt, pi
 
 N = 2
 d = 2
@@ -59,9 +59,104 @@ def mittelpunkt(f, a, b, N, d):
     b = np.array([b]*d, dtype=float)
 
     tmp = sum([f(a + (b-a)*(i-0.5)/N) for i in is_])
-    tmp *= (_b - _a) / float(N)
+    tmp *= (_b - _a) / float(N**d)
 
     return tmp
 
-if __name__ == '__main__':
-    print mittelpunkt(f, a=0, b=1, N=10, d=1)
+def hit_miss_monte(f, fmin, fmax, N, d, a, b):
+    _a = a
+    _b = b
+
+    a = np.array([a]*d, dtype=float)
+    b = np.array([b]*d, dtype=float)
+
+    N_plus = 0.0
+    rng = UniformRNG()
+
+    for _ in range(N**d):
+        ri = float(rng.next())
+        si = float(rng.next())
+        if f(a+(b-a)*ri) - fmin > (fmax-fmin)*si:
+            N_plus = N_plus + 1
+
+    return (_b - _a) * ((N_plus/float(N**d))*(fmax - fmin) + fmin)
+
+def stuetz_monteC_integr(f, N, d, a, b):
+    vorfaktor = (b - a)/float(N**d)
+    a = np.array([a]*d, dtype=float)
+    b = np.array([b]*d, dtype=float)
+
+    summe = 0.0
+    rng = UniformRNG()
+    for _ in range(N**d):
+        r = float(rng.next())
+        summe = summe + f(a + (b-a) * r)
+
+    return vorfaktor * summe
+
+import scipy.special
+
+def exact(d):
+    return ((sqrt(pi)/2) * scipy.special.erf(1))**d
+
+def calc_errors(N, ds):
+    mitt_errs = []
+    hm_errs = []
+    stu_errs = []
+    for d in ds:
+        mitt = mittelpunkt(f, a=0, b=1, N=N, d=d)
+        hm = hit_miss_monte(f, fmin=0, fmax=1, N=N, d=d, a=0, b=1)
+        stu = stuetz_monteC_integr(f, N=N, d=d, a=0, b=1)
+        ex = exact(d)
+
+        err = abs((mitt - ex)/ex)
+        mitt_errs.append((N, d, err))
+
+        err = abs((hm - ex)/ex)
+        hm_errs.append((N, d, err))
+
+        err = abs((stu - ex)/ex)
+        stu_errs.append((N, d, err))
+
+    return mitt_errs, hm_errs, stu_errs
+
+def plot_errors(mitt_errs, hm_errs, stu_errs, N):
+    plt.figure(0) # for the semilog plot
+
+    xs, ys = zip(*[(d, err) for N, d, err in mitt_errs])
+    plt.semilogy(xs, ys, label='Mittelpunkt, $N=%d$' % N)
+
+    xs, ys = zip(*[(d, err) for N, d, err in hm_errs])
+    plt.semilogy(xs, ys, label='Monte Carlo, $N=%d$' % N)
+
+    xs, ys = zip(*[(d, err) for N, d, err in stu_errs])
+    plt.semilogy(xs, ys, label=u'Stützstellen Monte Carlo, $N=%d$' % N)
+
+    plt.figure(1) # for the loglog plot
+
+    xs, ys = zip(*[(N**d, err) for N, d, err in mitt_errs])
+    plt.loglog(xs, ys, label='Mittelpunkt, $N=%d$' % N)
+
+    xs, ys = zip(*[(N**d, err) for N, d, err in hm_errs])
+    plt.loglog(xs, ys, label='Monte Carlo, $N=%d$' % N)
+
+    xs, ys = zip(*[(N**d, err) for N, d, err in stu_errs])
+    plt.loglog(xs, ys, label=u'Stützstellen Monte Carlo, $N=%d$' % N)
+
+
+plot_errors(*calc_errors(2, range(1, 18)), N=2)
+plot_errors(*calc_errors(10, range(1, 5)), N=10)
+
+plt.figure(0)
+plt.title('Relative Fehler bei der Integration, semilog')
+plt.xlabel(r'$d$')
+plt.ylabel('Relativer Fehler')
+plt.legend(loc='upper left')
+
+plt.figure(1)
+plt.title('Relative Fehler bei der Integration, loglog')
+plt.xlabel(r'$N^d$')
+plt.ylabel('Relativer Fehler')
+plt.legend(loc='upper left')
+
+plt.show()
