123cb098ef62b1e500ceed871555368f22a92f0b
Carlos Martín Nieto <cmn@dwim.me>
2014-01-30 22:00:40 +0100

    U13.2: peform calculations

--- u-13/u13_2.py
+++ u-13/u13_2.py
@@ -6,9 +6,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
-from math import exp
+from math import exp, sqrt, ceil
 from multiprocessing import Pool
-from itertools import product
+from itertools import product, groupby
 
 random.seed(5)
 
@@ -106,6 +106,16 @@ def magnetisation(field):
 
     return m
 
+# numpy.split() doesn't like it when we have lists as elements, so
+# let's put them in a class
+class FieldContainer:
+    def __init__(self):
+        self.fields = []
+
+    def append(self, field):
+        self.fields.append(field)
+    
+
 def simulate(n, kBT, H, steps, equilibrium, snapshot=False):
     """Simulate a field of size n**2"""
 
@@ -114,6 +124,7 @@ def simulate(n, kBT, H, steps, equilibrium, snapshot=False):
 
     Es = [] # history of the energy in the system
     Ms = [] # history or magnetisations
+    snaps = FieldContainer()
 
     for step in range(steps):
         # Generate a random pair, to see which spin we should try to
@@ -127,11 +138,18 @@ def simulate(n, kBT, H, steps, equilibrium, snapshot=False):
             E += dE
             flip_spin(field, i, j)
 
+        # Take a snapshot when we start counting and when we stop
+        if snapshot and step == equilibrium + 1:
+            snaps.append(field.copy())
+        if snapshot and step == steps -1:
+            snaps.append(field.copy())
+            
+            
         if step > equilibrium:
             Es.append(E)
             Ms.append(magnetisation(field))
 
-    return np.mean(Es), np.mean(Ms), np.mean(np.abs(Ms))
+    return np.mean(Es), np.mean(Ms), np.mean(np.abs(Ms)), snaps
 
 # Run our simulation, we get the variables via a tuple though Pool.map()
 def run_simulation((kBT, H)):
@@ -139,31 +157,84 @@ def run_simulation((kBT, H)):
     nsteps = 25000
     nequi = nsteps / 4 # the first fourth is to let it settle
 
-    return simulate(n, kBT, H, nsteps, nequi, False)
+    return simulate(n, kBT, H, nsteps, nequi, True)
 
 if __name__ == '__main__':
-
+    repeats = 50 # repeats per experiment
     kBTs = [1e-3, 1e-1, 1, 2, 5, 10, 100]
     Hs = [0, 0.1, 1]
     # Cartesian product of the temperatures and the magenic fields
     experiments = list(product(kBTs, Hs))
 
+    experiments = reduce(list.__add__, [[x]*repeats for x in experiments])      
+    print 'experiments', experiments
+    
     # Each experiment (and each repetition) is independent, so we can
     # create a pool of workers and make use of all the machine's
     # cores.
     pool = Pool()
     answers = pool.map(run_simulation, experiments)
 
-    Es, Ms, AMs = zip(*answers)
+    # The answers contain several copies of each experiment, so we
+    # need to split it up into groups to average them
+    deflattened = np.split(np.array(answers), len(kBTs)*len(Hs))
 
-    print experiments
-    print Es
+    # deflattened now has a bunch of arrays, each with tuples with the
+    # energy and magnetism, and we need to extract them
 
-    # Plot the energy for each magnetism level
+    Es = []
+    Ms = []
+    AMs = []
+    snaps = []
+    for ex in deflattened:
+        Es.append(np.mean(ex[:,0]))
+        Ms.append(np.mean(ex[:,1]))
+        AMs.append(np.mean(ex[:,2]))
+        snaps.append(ex[:,3])
+
+    print 'snaps', snaps
+    # Plot the energy
     for i, h in enumerate(Hs):
-        plt.semilogx(kBTs, Es[i::3], label='H = %g' % h)
+        plt.semilogx(kBTs, Es[i::3], label='<E>, H = %g' % h)
 
+    plt.title('Energie des Systems')
     plt.xlabel('$k_B T$')
     plt.ylabel('Energy')
     plt.legend()
-    plt.show()
+
+    plt.figure()
+    # Plot the magnetisms
+    for i, h in enumerate(Hs):
+        plt.semilogx(kBTs, Ms[i::3], label='<M> H = %g' % h)
+        plt.semilogx(kBTs, AMs[i::3], label='<|M|> H = %g' % h)
+
+    plt.title('Magnetismus des Systems')
+    plt.xlabel('$k_B T$')
+    plt.ylabel('<M>')
+    plt.legend()
+
+    # snaps is a list of FieldContainers (pairs)
+    for containers, h in zip(snaps, Hs):
+        side = ceil(sqrt(len(containers)))
+        plt.figure()
+        for i in range(len(containers)):
+            plt.subplot(side, side, i)
+            plt.xticks([])
+            plt.yticks([])
+            plt.imshow(containers[i].fields[0], extent=(0, 10, 10, 0), interpolation='nearest', cmap=plt.cm.jet)
+
+        plt.suptitle('Snapshot bei H %g, am Equilibrium' % h)
+        plt.savefig('snaps/h%s_equi.png' % (h))
+        plt.figure()
+        for i in range(len(containers)):
+            plt.subplot(side, side, i)
+            plt.xticks([])
+            plt.yticks([])
+            plt.imshow(containers[i].fields[1], extent=(0, 10, 10, 0), interpolation='nearest', cmap=plt.cm.jet)
+
+        
+        plt.suptitle('Snapshot bei H %g, am Ende' % h)
+        plt.savefig('snaps/h%s_ende.png' % (h))
+
+
+    #plt.show()
