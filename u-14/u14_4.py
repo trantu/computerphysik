da51fcd7d7a9a84f31ff096a98f7d7fadfe2a451
Carlos Martín Nieto <cmn@dwim.me>
2014-02-07 18:17:30 +0100

    U14.4: solve 3

--- u-14/u14_4.py
+++ u-14/u14_4.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import random
 from math import exp
 from math import factorial as fac
-from collections import Counter
+from collections import deque
 from multiprocessing import Pool
 import time
 
@@ -45,7 +45,7 @@ def count_edges(M):
 def count_conns(M, i):
     """Count the amount of connections which node 'i' has"""
 
-    # we can simply count non-false for the row/column
+    # we can simply count non-zero for the row/column
     return np.count_nonzero(M[i])
 
 def poisson(N, p):
@@ -56,11 +56,50 @@ def poisson(N, p):
     return np.random.poisson(lam=lam, size=N)
 
 def one_random((n, p)):
-        M = random_network(N, p)
-        c = [count_conns(M, i) for i in range(N)]
+        M = random_network(n, p)
+        c = [count_conns(M, i) for i in range(n)]
         return c
 
-if __name__ == '__main__':
+def neighbours(M, i):
+    """Return a list of *i*'s neighbours"""
+
+    ret = []
+    for j, val in enumerate(M[i,:]):
+        if val == 1:
+            ret.append(j)
+
+    return ret
+
+def distances(M, i):
+    # Keep a list of the nodes we've visited so we can avoid visiting
+    # them again
+    visited = [False] * len(M)
+    visited[i] = True # we've visited ourselves
+    
+    # Distance from us to a particular node
+    distance = [0] * len(M)
+    # Nodes we should visit, start with ourselves
+    q = deque([i])
+
+    while len(q) > 0:
+        k = q.popleft()
+        neigh = neighbours(M, k)
+
+        # Go through each of the node's neighbours which we've yet to
+        # visit
+        for n in neigh:
+            if visited[n]:
+                continue
+
+            # Mark as visited and set the distance to that node as the
+            # distance to the current node plus one
+            visited[n] = True
+            distance[n] = distance[k] + 1
+            q.append(n)
+
+    return distance
+
+def u14_4_2():
     N = 1000
     p = 0.02
 
@@ -81,4 +120,32 @@ if __name__ == '__main__':
     plt.hist(pois, normed=True, label='Poisson', alpha=0.5)
 
     plt.legend()
+    plt.show()
+
+def one_avg_distance((n, p)):
+    M = random_network(n, p)
+    return [distances(M, j) for j in range(n)]
+
+def average_distance(n, pN):
+    """Return the average distance, over five runs, of an n-node network"""
+    p = float(pN) / n
+
+    pool = Pool()
+    dist = pool.map(one_avg_distance, [(n,p)]*5)
+    return np.mean(dist)
+
+if __name__ == '__main__':
+    # u14_4_2()
+
+    Ns = range(60, 161, 20)
+
+
+    for pN in [10, 20]:
+        res = []
+        for N in Ns:
+            res.append(average_distance(N, pN))
+        plt.plot(Ns, res, label=r'$\langle l \rangle_N$, $pN = %d$' % (pN))
+
+    plt.semilogy(Ns, Ns, label=r'$\log N$')
+    plt.legend(loc='upper left')
     plt.show()
